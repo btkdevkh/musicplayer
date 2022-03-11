e677834f204c585b07aea17eabd6ab0545843739
btkdevkhdelli5 <amcjkbtk91@outlook.com>
2022-03-11 01:11:13 +0100

    App Version to 1.0.3

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,11 +2,7 @@ const Footer = () => {
   return (
     <footer>
       <div>
-        <p>Btkdevkh Music Player</p>
-        <hr />
-        <p><small><i className="fab fa-react"></i> Hit play & Relax <i className="fab fa-react"></i></small></p>
-        <p style={{marginTop: "10px"}}><small>App Version 1.0.2</small></p>
-        <p><small>Thanks, <a style={{color: "#61dafb"}} href="https://btkdevkh.netlify.app/" target="_blank" rel="noreferrer">Btkdevkh</a></small></p>
+        <p><small>App Version 1.0.3 &copy; {new Date().getFullYear()}</small></p>
       </div>
     </footer>
   )
