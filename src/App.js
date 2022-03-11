11f14a350fe5a8eb69e3e342b92200ce4ab22abf
btkdevkhdelli5 <amcjkbtk91@outlook.com>
2022-03-11 13:32:09 +0100

    App Version to 1.0.7

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Switch, Route } from 'react-router-dom';
-import Header from './components/Header';
+// import Header from './components/Header';
 import Home from './pages/Home';
-import Footer from './components/Footer';
+// import Footer from './components/Footer';
 import './assets/css/App.css';
 
 const App = () => {
