7417e2c1c57565d2d3fd9e4b9964f5a4da431adf
Bunthoeun Kong <amcjkbtk91@outlook.com>
2025-04-10 17:04:05 +0200

    Remove fake CD

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,21 @@
-import Player from "../components/Player"
-import cambodia from "../assets/img/cambodia.png"
-import france from "../assets/img/france.png"
-import { useState } from "react"
+import Player from "../components/Player";
+import cambodia from "../assets/img/cambodia.png";
+import france from "../assets/img/france.png";
+import { useState } from "react";
 
 const Home = () => {
-  const [index, setIndex] = useState(0)
-  const [isPlay, setIsPlay] = useState(false)
+  const [index, setIndex] = useState(0);
+  const [isPlay, setIsPlay] = useState(false);
 
   return (
     <main className="main-bg">
-      <div className="music-fake">
+      {/* <div className="music-fake">
         <div className={"music-info play"}>
           <div className="cover">
             <img src={cambodia} alt="album-cover" />
           </div>
         </div>
-      </div>
+      </div> */}
 
       <Player
         index={index}
@@ -24,15 +24,15 @@ const Home = () => {
         setIsPlay={setIsPlay}
       />
 
-      <div className="music-fake">
+      {/* <div className="music-fake">
         <div className="music-info play">
           <div className="cover">
             <img src={france} alt="album-cover" />
           </div>
         </div>
-      </div>
+      </div> */}
     </main>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
